76b57dee1c26b2fd6126c4bf1394d345109a46eb
Tomás Segura <38998436+toms99@users.noreply.github.com>
2024-12-27 20:35:42 -0600

    Update streamlit_app.py
    adding requests lib

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("🥤Customize Your Smoothie!🥤")
@@ -43,4 +44,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
         
-
+smoothiefroot_response  = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
